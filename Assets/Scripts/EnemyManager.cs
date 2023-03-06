7d007ef9651cad1d6f1e1666c214c41cf941e8ca
Joseph Rivera <josrivera@csumb.edu>
2023-03-05 23:53:04 -0800

    Part 1

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject redEnemy;
+    public GameObject blueEnemy;
+    public GameObject greenEnemy;
+    public GameObject yellowEnemy;
+}
